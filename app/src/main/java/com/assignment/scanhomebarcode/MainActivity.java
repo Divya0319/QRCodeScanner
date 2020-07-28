a2b2e6fb9c8a3aae5f55f726f03f4c61d884b546
Divya Gupta <divygupta0319@gmail.com>
2020-07-28 11:02:35 +0530

    Scan again button added, some minor optimisations done

--- app/src/main/java/com/assignment/scanhomebarcode/MainActivity.java
+++ app/src/main/java/com/assignment/scanhomebarcode/MainActivity.java
@@ -4,7 +4,8 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +16,20 @@ import com.google.android.gms.vision.barcode.Barcode;
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_CAMERA = 344;
+    private TextView statusTV, qrCodeTV;
+    private Button btScanAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusTV = findViewById(R.id.status_tv);
+        qrCodeTV = findViewById(R.id.qrcode_tv);
+        btScanAgain = findViewById(R.id.scan_again_bt);
+
+        btScanAgain.setOnClickListener(v -> {
+            initiateScan();
+        });
 
         initiateScan();
 
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void onQrCodeScanned(String result) {
         Log.d("QR scanned:", result);
+        qrCodeTV.setText("QR code data: " + result);
         runOnUiThread(() -> showDropDown(result));
 
     }
@@ -55,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
     private void showDropDown(String qrData) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Select an option for " + qrData);
+        builder.setCancelable(false);
         String[] options = {"---Select---", "Collected", "Available", "Not Available"};
         builder.setItems(options, (dialog, which) -> {
             dialog.dismiss();
@@ -76,15 +88,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onNotAvailableSelected() {
-        Toast.makeText(this, "Not Available Selected", Toast.LENGTH_LONG).show();
+        statusTV.setText(getString(R.string.status_not_available));
     }
 
     private void onAvailableSelected() {
-        Toast.makeText(this, "Available Selected", Toast.LENGTH_LONG).show();
+        statusTV.setText(getString(R.string.status_available));
     }
 
     private void onCollectedSelected() {
-        Toast.makeText(this, "Collected Selected", Toast.LENGTH_LONG).show();
+        statusTV.setText(getString(R.string.status_collected));
     }
 
 }
